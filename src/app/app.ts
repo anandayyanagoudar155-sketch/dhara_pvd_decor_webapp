4d54874824665c8551415f318fe116a1924f3486
unknown <anandayyanagoudar155@gmail.com>
2025-11-27 13:23:05 +0530

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'dhara_pvd_decor_webapp';
+}
